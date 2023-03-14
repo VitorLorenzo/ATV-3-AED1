83bfa138f8d0a2ab45a340641757cc6c583b1a05
Vitor Lorenzo <81115033+VitorLorenzo@users.noreply.github.com>
2023-03-14 19:09:17 -0300

    Add files via upload
    TESTE

---
+++ atv3_AED1/atv3_AED1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Calculo_Salario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario p1;
+            p1 = new Salario();
+
+            Console.WriteLine("Digite o nome:");
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.WriteLine("Digite seu salario:");
+            p1.Salar = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Digite seu total de vendas:");
+            p1.bonus = double.Parse(Console.ReadLine("F4", CultureInfo.InvariantCulture)());
+            p1.Montante = double.Parse(Console.ReadLine());
+            Console.WriteLine();   
+
+
+            p1.ImprimirDados();
+            p1.calcularTotal();
+
+
+
+        }
+    }
+}
---
+++ atv3_AED1/atv3_AED1/Salario.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Salario
+{
+    internal class Salario
+    {
+        private string nom;
+        private double sal;
+        private double mont;
+        private double bonus;
+
+       public string Nome
+        {
+            get
+            {
+                return nom;
+            }
+            set
+            {
+                nom = value;
+            }
+        }
+
+        public double Salar
+        {
+            get
+            {
+                return sal;
+            }
+            set
+            {
+                sal = value;
+            }
+        }
+
+        public double Montante
+        {
+            get
+            {
+                return mont;
+            }
+            set
+            {
+                mont = value;
+            }
+        }
+         public double bonus
+        {
+            get
+            {
+                return bonus;
+            }
+            set
+            {
+                bonus = value;
+            }
+        }
+       
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + nom);
+            Console.WriteLine();
+            Console.WriteLine("Salario: " + sal);
+            Console.WriteLine();
+            Console.WriteLine("Bonus: " + Total)
+            
+
+        }
+
+
+        public void calcularTotal()
+        {
+            mont = (15 / 100) * bonus;
+
+            Console.WriteLine("Total: " + bonus);
+            Console.WriteLine();
+        }
+
+    }
+}
